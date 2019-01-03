4532d1aaeeff2f195ce785a531a42f681967fff5
Yuhang Li <18326007+WilliamYuhangLee@users.noreply.github.com>
2019-01-03 18:00:03 +0800

    Add slp.h/.c

---
+++ src/slp.c
@@ -0,0 +1,79 @@
+/**
+ * Created by William Lee on 1/3/19.
+ *
+ * Defines the data structures used in a straight-line program
+ */
+
+#include "slp.h"
+
+// Constructors for A_statement's
+A_statement A_CompoundStatement(A_statement statement1, A_statement statement2) {
+    A_statement p = checked_malloc(sizeof(A_statement));
+    p->kind = A_compoundStatement;
+    p->u.compound.statement1 = statement1;
+    p->u.compound.statement2 = statement2;
+    return p;
+}
+
+A_statement A_AssignStatement(string id, A_expression expression) {
+    A_statement p = checked_malloc(sizeof(A_statement));
+    p->kind = A_assignStatement;
+    p->u.assign.id = id;
+    p->u.assign.expression = expression;
+    return p;
+}
+
+A_statement A_PrintStatement(A_expressionList expressions) {
+    A_statement p = checked_malloc(sizeof(A_statement));
+    p->kind = A_printStatement;
+    p->u.print.expressions = expressions;
+    return p;
+}
+
+// Constructors for A_expression's
+A_expression A_IdExpression(string id) {
+    A_expression p = checked_malloc(sizeof(A_expression));
+    p->kind = A_idExpression;
+    p->u.id = id;
+    return p;
+}
+
+A_expression A_NumExpression(int num) {
+    A_expression p = checked_malloc(sizeof(A_expression));
+    p->kind = A_numExpression;
+    p->u.num = num;
+    return p;
+}
+
+A_expression A_OpExpression(A_expression left, A_binop operator, A_expression right) {
+    A_expression p = checked_malloc(sizeof(A_expression));
+    p->kind = A_opExpression;
+    p->u.op.left = left;
+    p->u.op.operator = operator;
+    p->u.op.right = right;
+    return p;
+}
+
+A_expression A_ExpressionSequenceExpression(A_statement statement, A_expression expression) {
+    A_expression p = checked_malloc(sizeof(A_expression));
+    p->kind = A_expressionSequenceExpression;
+    p->u.expressionSequence.statement = statement;
+    p->u.expressionSequence.expression = expression;
+    return p;
+}
+
+// Constructors for A_expressionList's
+A_expressionList A_PairExpressionList(A_expression head, A_expressionList tail) {
+    A_expressionList p = checked_malloc(sizeof(A_expressionList));
+    p->kind = A_pairExpressionList;
+    p->u.pair.head = head;
+    p->u.pair.tail = tail;
+    return p;
+}
+
+A_expressionList A_LastExpressionList(A_expression last) {
+    A_expressionList p = checked_malloc(sizeof(A_expressionList));
+    p->kind = A_lastExpressionList;
+    p->u.last = last;
+    return p;
+}
---
+++ src/slp.h
@@ -0,0 +1,91 @@
+/**
+ * Created by William Lee on 1/3/19.
+ *
+ * Declares the data structures used in a straight-line program.
+ */
+
+#ifndef TCC_SLP_H
+#define TCC_SLP_H
+
+#include "util.h"
+
+typedef struct A_statement_ *A_statement;
+typedef struct A_expression_ *A_expression;
+typedef struct A_expressionList_ *A_expressionList;
+typedef enum {
+    A_plus,
+    A_minus,
+    A_times,
+    A_div
+} A_binop;
+
+struct A_statement_ {
+    enum {
+        A_compoundStatement,
+        A_assignStatement,
+        A_printStatement
+    } kind;
+    union {
+        struct {
+            A_statement statement1;
+            A_statement statement2;
+        } compound;
+        struct {
+            string id;
+            A_expression expression;
+        } assign;
+        struct {
+            A_expressionList expressions;
+        } print;
+    } u;
+};
+
+A_statement A_CompoundStatement(A_statement statement1, A_statement statement2);
+A_statement A_AssignStatement(string id, A_expression expression);
+A_statement A_PrintStatement(A_expressionList expressions);
+
+struct A_expression_ {
+    enum {
+        A_idExpression,
+        A_numExpression,
+        A_opExpression,
+        A_expressionSequenceExpression
+    } kind;
+    union {
+        string id;
+        int num;
+        struct {
+            A_expression left;
+            A_binop operator;
+            A_expression right;
+        } op;
+        struct {
+            A_statement statement;
+            A_expression expression;
+        } expressionSequence;
+    } u;
+};
+
+A_expression A_IdExpression(string id);
+A_expression A_NumExpression(int num);
+A_expression A_OpExpression(A_expression left, A_binop operator, A_expression right);
+A_expression A_ExpressionSequenceExpression(A_statement statement, A_expression expression);
+
+struct A_expressionList_ {
+    enum {
+        A_pairExpressionList,
+        A_lastExpressionList
+    } kind;
+    union {
+        struct {
+            A_expression head;
+            A_expressionList tail;
+        } pair;
+        A_expression last;
+    } u;
+};
+
+A_expressionList A_PairExpressionList(A_expression head, A_expressionList tail);
+A_expressionList A_LastExpressionList(A_expression last);
+
+#endif //TCC_SLP_H
